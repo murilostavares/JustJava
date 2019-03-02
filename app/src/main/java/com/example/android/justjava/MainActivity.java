8b293043ec0ea1fc752c377a8eddadbd16459090
murilostavares <murilo@stavares.com>
2019-03-02 02:58:57 -0300

    Versão Correta (2.0)

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -19,14 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view) {
 
-        double price = calcularPreco();
-        displayMessage(resumoPedido(price));
+        CheckBox chantilly = findViewById(R.id.checkbox_chantilly);
+        boolean hasChantilly = chantilly.isChecked();
+
+        CheckBox chocolate = findViewById(R.id.checkbox_chocolate);
+        boolean hasChocolate = chocolate.isChecked();
+
+        double price = calcularPreco(hasChantilly,hasChocolate);
+        displayMessage(resumoPedido(price, hasChantilly, hasChocolate));
     }
 
-    private String resumoPedido(double price) {
+    private String resumoPedido(double price, boolean hasChantilly, boolean hasChocolate) {
 
-        String name = "Murilo de Souza Tavares";
-        String message = "Nome: " + name +
+        EditText name = findViewById(R.id.nome_view);
+        String nome = name.getText().toString();
+
+        String chantillySN = "SIM";
+        String chocolateSN = "SIM";
+
+        if (hasChantilly == false) {
+            chantillySN = "NÃO";
+        }
+        if (hasChocolate == false) {
+            chocolateSN = "NÃO";
+        }
+
+        String message = "Nome: " + nome +
+                "\nChantilly? :" + chantillySN +
+                "\nChocolate? :" + chocolateSN +
                 "\nQuantidade: " + num +
                 "\nTotal: R$" + price +
                 "\nObrigado!";
@@ -34,9 +56,22 @@ public class MainActivity extends AppCompatActivity {
         return message;
     }
 
-    private int calcularPreco() {
+    private int calcularPreco(boolean hasChantilly, boolean hasChocolate) {
 
-        int calculo = num * precoUnitario;
+        int chantillyPreco = 5;
+        int chocolatePreco = 5;
+
+        if (hasChantilly == false) {
+
+            chantillyPreco = 0;
+        }
+
+        if (hasChocolate == false) {
+
+            chocolatePreco = 0;
+        }
+
+        int calculo = num * precoUnitario + chantillyPreco + chocolatePreco;
 
         return calculo;
     }
@@ -49,19 +84,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void decrement(View view) {
 
-        num--;
+        if (num > 0) {
+
+            num--;
+        } else {
+
+            num = 0;
+        }
+
+
         displayQuantity(num);
+
     }
 
     private void displayQuantity(int number) {
 
-        TextView quantityTextView = (TextView) findViewById(R.id.quantidade_apresentada);
+        TextView quantityTextView = findViewById(R.id.quantidade_apresentada);
         quantityTextView.setText("" + number);
     }
 
     private void displayMessage(String message) {
 
-        TextView pedidoTextView = (TextView) findViewById(R.id.pedido_apresentado);
+        TextView pedidoTextView = findViewById(R.id.pedido_apresentado);
         pedidoTextView.setText(message);
     }
 
